8822dd552017b62de1f39fc34fd11724ec06edc1
NoE <3504893952@qq.com>
2024-08-25 22:39:46 +0800

    update structure

--- cmake/Utils.cmake
+++ cmake/Utils.cmake
@@ -1,2 +1,16 @@
 # Utils
 # 
+
+# ==================================================
+# Get subdirectories of current path.
+function(get_subdirs cur_path out_dirs)
+    set(out_list "")
+    file(GLOB children LIST_DIRECTORIES true "${cur_path}/*")
+    foreach(child IN LISTS children)
+        if(IS_DIRECTORY ${child})
+            list(APPEND out_list ${child})
+        endif()
+    endforeach()
+    set(${out_dirs} ${out_list} PARENT_SCOPE)
+endfunction()
+
