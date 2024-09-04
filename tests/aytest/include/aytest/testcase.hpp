2e515da19660acd142c262b4dcb5fc92638940ec
NoE <3504893952@qq.com>
2024-09-04 12:48:26 +0800

    update test case

--- tests/aytest/include/aytest/testcase.hpp
+++ tests/aytest/include/aytest/testcase.hpp
@@ -4,13 +4,44 @@
 #include <string>
 #include <functional>
 
-namespace test {
+namespace aytest {
 
 struct TestCase {
     std::string name_;
-    std::function<void(void)> case_;
+
+    std::function<void(TestCase *)> fn_case_;
+
+    std::size_t cnt_exec_ = 0;
+    std::size_t cnt_pass_ = 0;
+    std::size_t cnt_fail_ = 0;
+
+    bool run() {
+        reset();
+        if (!fn_case_) {
+            return false;
+        }
+        try {
+            fn_case_(this);
+        } catch(...) {
+            return false;
+        }
+        return true;
+    }
+
+    void reset() {
+        cnt_exec_ = 0;
+        cnt_pass_ = 0;
+        cnt_fail_ = 0; 
+    }
 };
 
 }
+
+template <>
+struct std::hash<aytest::TestCase> {
+    size_t operator()(aytest::TestCase const & _tc) const {
+        return std::hash<std::string>{}(_tc.name_);
+    }
+};
 
 #endif
